b084857decf2f35dee6a45d8fedf3e42df460d34
blueberryfox <nastja1905@mail.ru>
2018-03-10 06:19:29 +0500

    fixed singer list

--- ShowSinger/app/src/main/java/app/bluberryfox/showsinger/ui/SingerInfo.kt
+++ ShowSinger/app/src/main/java/app/bluberryfox/showsinger/ui/SingerInfo.kt
@@ -3,6 +3,8 @@ package app.bluberryfox.showsinger.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.widget.ImageView
 import app.bluberryfox.showsinger.R
 
 /**
@@ -13,9 +15,16 @@ class SingerInfo: AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.singer_info)
         var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        var img = findViewById<ImageView>(R.id.imageView)
+        img.setImageResource(R.drawable.album1)
 
         toolbar.setNavigationIcon(R.drawable.ic_keyboard_arrow_left_white)
         setSupportActionBar(toolbar)
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_view, menu)
+        return true
+    }
+
 
 }
