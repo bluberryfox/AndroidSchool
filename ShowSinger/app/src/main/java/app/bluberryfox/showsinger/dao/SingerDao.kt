b1ebebe470be17916f1e2951b98481dd06401bb2
blueberryfox <nastja1905@mail.ru>
2018-04-05 19:53:44 +0500

    deleted favorite button

--- ShowSinger/app/src/main/java/app/bluberryfox/showsinger/dao/SingerDao.kt
+++ ShowSinger/app/src/main/java/app/bluberryfox/showsinger/dao/SingerDao.kt
@@ -2,6 +2,7 @@ package app.bluberryfox.showsinger.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
 import app.bluberryfox.showsinger.data.Singer
 
@@ -10,6 +11,6 @@ interface SingerDao {
     @Query("SELECT * FROM Singer")
     fun getAll():List<Singer>
 
-    @Insert
+    @Insert(onConflict = REPLACE)
     fun insertAll(singers:List<Singer>)
 }
